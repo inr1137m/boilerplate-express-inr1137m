fbaab5d4295576323095d15754d92c5010adc2a8
Iyyanar Madeshvaran <inr1137m@gmail.com>
2021-12-17 23:08:39 +0530

    Chained middlewares

--- myApp.js
+++ myApp.js
@@ -2,6 +2,25 @@ var express = require('express');
 var app = express();
 
 console.log('Hello World');
+app.get('/', (req,res) => {
+	res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.get('/now', (req,res,next) => {
+	req.time = new Date().toString();
+	next();	
+}, (req,res) => {
+	res.send({time:req.time});
+});
+
+app.get('/json', (req,res) => {
+const mySecret = process.env['MESSAGE_STYLE'];
+if(mySecret == 'uppercase'){
+  res.json({"message":"HELLO JSON"});
+} else {
+  res.json({"message":"Hello json"});
+}
+});
 
 
 
--- server.js
+++ server.js
@@ -29,26 +29,6 @@ app.use('/json', (req,res,next) => {
 });
 
 
-app.get('/', (req,res) => {
-	res.sendFile(`${__dirname}/views/index.html`);
-});
-
-app.get('/now', (req,res,next) => {
-	req.time = new Date().toString();
-	next();	
-}, (req,res) => {
-	res.send({time:req.time});
-});
-
-app.get('/json', (req,res) => {
-const mySecret = process.env['MESSAGE_STYLE'];
-if(mySecret == 'uppercase'){
-  res.json({"message":"HELLO JSON"});
-} else {
-  res.json({"message":"Hello json"});
-}
-});
-
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
   bGround.log('Node is listening on port '+ port + '...')
