3fa0030adff05b867cb3be9fc7387b90d805c308
Iyyanar Madeshvaran <inr1137m@gmail.com>
2021-12-17 22:57:38 +0530

    Chained middlewares

--- server.js
+++ server.js
@@ -28,15 +28,16 @@ app.use('/json', (req,res,next) => {
 	next();
 });
 
+
+app.get('/', (req,res) => {
+	res.sendFile(`${__dirname}/views/index.html`);
+});
+
 app.get('/now', (req,res,next) => {
 	req.time = new Date().toString();
 	next();	
 }, (req,res) => {
 	res.send({time:req.time});
-});
-
-app.get('/', (req,res) => {
-	res.sendFile(`${__dirname}/views/index.html`);
 });
 
 app.get('/json', (req,res) => {
