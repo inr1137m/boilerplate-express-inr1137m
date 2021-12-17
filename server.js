e0129935c8e5e138daa9f3e5c5ca230b32fc33af
Iyyanar Madeshvaran <inr1137m@gmail.com>
2021-12-17 22:44:33 +0530

    Added request logger middleware

--- server.js
+++ server.js
@@ -23,12 +23,17 @@ if (!process.env.DISABLE_XORIGIN) {
 
 app.use('/public', express.static(`${__dirname}/public`));
 
+app.use('/json', (req,res,next) => {
+	console.log(req.method+" "+req.path+" - "+req.ip);
+	next();
+});
+
 app.get('/', (req,res) => {
 	res.sendFile(`${__dirname}/views/index.html`);
 });
 
 app.get('/json', (req,res) => {
-const mySecret = process.env['MESSAGE_STYLE']
+const mySecret = process.env['MESSAGE_STYLE'];
 if(mySecret == 'uppercase'){
   res.json({"message":"HELLO JSON"});
 } else {
