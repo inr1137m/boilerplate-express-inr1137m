e2d58751fd6c85bf1b5689984f10ca2c209051f1
Iyyanar Madeshvaran <inr1137m@gmail.com>
2021-12-17 20:58:22 +0530

    Route to serve JSON object

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/', (req,res) => {
 	res.sendFile(`${__dirname}/views/index.html`);
 });
 
-app.get)('/json', (req,res) => {
+app.get('/json', (req,res) => {
 	res.json({"message":"Hello json"});
 });
 
