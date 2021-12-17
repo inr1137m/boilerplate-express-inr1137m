d11bb189322e75bcec8caf52d022002f64a970d7
Iyyanar Madeshvaran <inr1137m@gmail.com>
2021-12-17 23:30:41 +0530

    Installed body parser

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use('/public', express.static(`${__dirname}/public`));
 
@@ -8,17 +9,19 @@ app.use('/json', (req,res,next) => {
 	next();
 });
 
-console.log('Hello World');
-
-app.get('/', (req,res) => {
-	res.sendFile(`${__dirname}/views/index.html`);
-});
-
 app.get('/now', (req,res,next) => {
 	req.time = new Date().toString();
 	next();	
 }, (req,res) => {
 	res.send({time:req.time});
+});
+
+app.use('/', bodyParser.urlencoded({extended:false}));
+
+console.log('Hello World');
+
+app.get('/', (req,res) => {
+	res.sendFile(`${__dirname}/views/index.html`);
 });
 
 app.get('/json', (req,res) => {
@@ -34,10 +37,9 @@ app.get('/:word/echo', (req,res) => {
 	res.json({echo:req.params.word});
 });
 
-app.post('/name',(req,res) => {
+app.route('/name').get((req,res) => {
 	res.json({name:`${req.query.first} ${req.query.last}`});
 });
-
 
 
 
